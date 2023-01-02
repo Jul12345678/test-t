dd323f842c122409394901bbea783fafb97aa6eb
Julian Schoepf <julian@codeversity.com>
2023-01-02 11:31:10 +0100

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Box, Text, Link, VStack, Code, Grid } from "@chakra-ui/react";
+
+export const App = () => (
+  <Box textAlign="center" fontSize="xl">
+    <Grid minH="100vh" p={3}>
+      <VStack spacing={8}>
+        <Text>Start Project</Text>
+      </VStack>
+    </Grid>
+  </Box>
+);
